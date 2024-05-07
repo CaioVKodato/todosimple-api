96d03be7253a1b40f8fec90247618738c8283ab5
CaioVKodato <caiovictorkodatot@gmail.com>
2024-05-07 01:13:01 -0300

    Adicionando JsonProperty

--- src/main/java/com/caiokodato/todosimple/models/User.java
+++ src/main/java/com/caiokodato/todosimple/models/User.java
@@ -10,6 +10,11 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import java.util.Objects;
+
 @Entity
 @Table(name = User.TABLE_NAME)
 
@@ -25,24 +30,24 @@ public class User {
     }
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    @Column (name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Long id;
 
-    @Column (name = "username", length = 100, nullable = false, unique = true)
-    @NotNull (groups = CreateUser.class)
-    @NotEmpty (groups = CreateUser.class)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
-    @Column (name = "password", length = 60, nullable = false)
-    @NotNull (groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty (groups = {CreateUser.class, UpdateUser.class})
-    @Size (groups = {CreateUser.class,UpdateUser.class}, min = 8, max = 60)
+    @JsonProperty (access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
-
+    // private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -77,8 +82,35 @@ public class User {
         this.password = password;
     }
 
-    
+    @Override
+    public boolean equals(Object obj) {
 
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
 
+        User other = (User) obj;
+        if (this.id == null)
+            if (other.id != null)
+                return false;
+            else if (!this.id.equals(other.id))
+                return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
 
 }
